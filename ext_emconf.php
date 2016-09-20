300200b432983f98e7d036d53121f1962ed1527c
Patrick Broens <patrick@patrickbroens.nl>
2016-09-20 12:23:01 +0200

    [TASK] Change version number from 1.1.2 to 1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 $EM_CONF['url_forwarding'] = [
     'title' => 'URL Forwarding',
-    'description' => 'Redirects (301, 302, 303, 307) to internal/external page or file, based on domain and path',
+    'description' => 'Redirects (301, 302, 303, 307) to internal/external page, file or partial path replacement, based on domain and path',
     'category' => 'fe',
     'state' => 'stable',
     'author' => 'Patrick Broens',
     'author_email' => 'patrick@patrickbroens.nl',
-    'version' => '1.1.2',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-7.6.99'
